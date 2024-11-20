94ec4f3d980043c5253b3bdf8af0a21d58922164
0bubble0 <129824619+0bubble0@users.noreply.github.com>
2024-11-20 22:09:06 +0800

    Delete receive_data.php

--- receive_data.php
+++
@@ -1,7 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $message = isset($_POST["message"])? $_POST["message"] : "";
-    // 这里简单地将接收到的数据返回给请求的客户端（用于在网页上显示），实际可以存储到数据库等做更多处理
-    echo $message; 
-}
-?>
